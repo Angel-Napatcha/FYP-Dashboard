7cfee325988e7f86badf21cc06330e9feb88d70d
Angel-Napatcha <113200583+Angel-Napatcha@users.noreply.github.com>
2024-04-08 03:41:25 +0700

    Formatted Attendance Graph (without toggle)

--- Attendance Dashboard/utils.py
+++ Attendance Dashboard/utils.py
@@ -115,25 +115,26 @@ def create_summary_section(df):
     summary_section = dbc.Container([
         html.H6("Summary", style={
             'text-align': 'left',
-            'margin-bottom': '0.25em',
+            'margin-bottom': '0.5em',
             'margin-left': '0.5em',
-            'margin-top': '0.5em'
+            'margin-top': '0.75em'
         }),
         dbc.Row(
             summary_content,
             justify="center",
             )
-        ], fluid=True, className="summary-section")       
+        ], fluid=True, className="summary-container")       
     
     return summary_section
 
 def create_enrolment_graph(df, level_of_study):
     if level_of_study == 'UG':
         colors = ['#7BBC9A', '#478DB8', '#E9BA5D', '#E46E53', '#9C71C6', '#AF7C4F']
-        xaxis_range = [-8, 225]
-        bar_height = 30
+        xaxis_range = [-8, 200]
+        bar_height = 29
         enrolment_id = 'ug-enrolment-graph'
         year_level = 'Year'
+        
     elif level_of_study == 'PGT':
         colors = ['#478DB8', '#E9BA5D']
         xaxis_range = [-2, 50]
@@ -143,7 +144,7 @@ def create_enrolment_graph(df, level_of_study):
 
     enrolment_data = calculate_student_enrolment(df, level_of_study)
     num_courses = len(enrolment_data['total_students_per_course'])
-    graph_height = num_courses * (bar_height) # bar_padding can be adjusted if needed
+    graph_height = num_courses * (bar_height)
     
     data = []
     cumulative_sums = {key: 0 for key in enrolment_data['total_students_per_course'].keys()}
@@ -174,14 +175,33 @@ def create_enrolment_graph(df, level_of_study):
 
     layout = go.Layout(
         barmode='stack',
-        yaxis={'automargin': True, 'autorange': 'reversed', 'tickfont': {'size': 12}},
-        xaxis={'range': xaxis_range, 'tickfont': {'size': 12}},
-        bargap=0.30, bargroupgap=0.25, height=graph_height, showlegend=False,
-        plot_bgcolor='rgba(0,0,0,0)', margin=dict(l=50, r=30, t=15, b=15)
+        yaxis={
+            'automargin': True,
+            'autorange': 'reversed',
+        },
+        xaxis={
+            'range': xaxis_range,
+        },
+        bargap=0.30,
+        bargroupgap=0.25,
+        height=graph_height,
+        showlegend=False,
+        plot_bgcolor='rgba(0,0,0,0)',
+        margin=dict(l=50, r=30, t=15, b=15),
+        font=dict(family='sans-serif', size=12)  # Default font for the whole layout
     )
 
     enrolment_figure = go.Figure(data=data, layout=layout)
-    enrolment_graph = dcc.Graph(id=enrolment_id, figure=enrolment_figure, style={'maxHeight': '450px', 'border-radius': '15px', 'overflowY': 'scroll', 'width': '100%'})
+    enrolment_graph = dcc.Graph(
+        id=enrolment_id, 
+        figure=enrolment_figure, 
+        style={
+            'maxHeight': '390px', 
+            'border-radius': '15px', 
+            'overflowY': 'scroll', 
+            'width': '100%'
+            }
+        )
 
     # Legends
     year_levels = [f'Year {i}' for i in range(len(colors))]
@@ -247,8 +267,7 @@ def create_enrolment_section(df):
                 id='pgt-enrolment-content', 
                 style={'display': 'none'} 
             )
-        ],
-        className='enrolment-container'
+        ]
     )
     return enrolment_section
 
@@ -263,9 +282,9 @@ def create_attendance_graph(df, level_of_study, year_of_course):
     colors = ['#7252A7', '#9099FF', '#6EB1FF', '#9CDBFF']
 
     # Constants for bar dimensions and spacing
-    bar_width = 0.15  # Fixed width for each bar
+    bar_width = 0.1   # Fixed width for each bar
     space_between_bars = 0.03
-    space_between_groups = 0.2
+    space_between_groups = 0.15
 
     # Calculate x positions for each bar and adjust the graph width
     x_positions = []
@@ -309,16 +328,13 @@ def create_attendance_graph(df, level_of_study, year_of_course):
 
     # Layout configuration
     layout = go.Layout(
-        yaxis= 
-        dict(
-            title=dict(
-                text='Attendance (%)',
-                font=dict(
-                    family='sans-serif'
-                ),
+        yaxis=dict(
+            title='Attendance (%)',
+            titlefont=dict(
+                family='sans-serif',
             ),
             tickfont=dict(
-                family='sans-serif'               
+                family='sans-serif',             
             ),
             range=[0, 100],
             dtick=25,
@@ -326,17 +342,18 @@ def create_attendance_graph(df, level_of_study, year_of_course):
         ),
         xaxis=dict(
             tickfont=dict(
-                family='sans-serif'               
+                family='sans-serif',
+                size=12,             
             ),
-            tickvals=[sum(pos)/len(pos) for pos in x_positions],
+            tickvals=[sum(pos) / len(pos) for pos in x_positions],
             ticktext=courses,
-            range = x_axis_range
+            range=x_axis_range,
         ),
         barmode='group',
-        height=325,
+        height=210,
         showlegend=False,
-        plot_bgcolor='rgba(0,0,0,0)',
-        margin=dict(l=50, r=50, t=50, b=100),
+        plot_bgcolor='#F7F7F7',
+        margin=dict(l=75, r=20, t=35, b=35),
         autosize=False,
         width=graph_width  # Dynamically adjusted width
     )
@@ -352,15 +369,67 @@ def create_attendance_graph(df, level_of_study, year_of_course):
             'overflowX': 'auto',  # Allows horizontal scrolling if needed
             'overflowY': 'hidden',  # Prevents vertical scrolling
             'width': '100%',
-            'minWidth': f'{graph_width}px',  # Ensures the graph width is dynamically set
+            'maxWidth': '25.5em',  # Ensures the graph width is dynamically set
         }
     )
+    
+    attendance_legend = html.Div(
+        [html.Div(
+            [
+                html.Div(style={'background-color': colors[i], 'width': '10px', 'height': '10px', 'border-radius': '50%'}, className='legend-circle'),
+                html.Div(f'Week {i*3+1}-{i*3+3}', className='legend-text')
+            ],
+            className='legend-entry'
+        ) for i in range(num_quarters)],
+        className='legend-column'
+    )
 
-    return attendance_graph
+    return attendance_graph, attendance_legend
 
 def create_attendance_section(df):
     
-    return
+    attendance_graph, attendance_legend = create_attendance_graph(df, 'UG', 1)
+    
+    attendance_content = html.Div(
+        [
+            html.Div(
+                attendance_legend,
+                className='attendance-legend'
+            ),
+            dbc.Col(
+                html.Div(
+                    attendance_graph,
+                    className='attendance-graph-wrapper'
+                )
+            ),
+        ],
+        className='attendance-content'
+    )
+    
+    attendance_section = html.Div(
+        [
+            dbc.Row(
+                html.H6("Student Attendance", style={
+                    'text-align': 'left',
+                    'margin-bottom': '1em',
+                    'margin-top': '0.5em'
+                }),
+                justify="start",
+                align="center"
+            ),
+            html.Div(
+                [
+                   attendance_content
+                ],
+                id='attendance-content', 
+                style={'display': 'block'} 
+            )
+        ],
+        className='attedance-container '
+    )
+            
+
+    return attendance_section
 
 def parse_contents(contents, filename, date):
     content_type, content_string = contents.split(',')
@@ -375,15 +444,20 @@ def parse_contents(contents, filename, date):
             
             summary_section = create_summary_section(df)
             enrolment_section = create_enrolment_section(df)
-            attendance_section = create_attendance_graph(df, 'PGT', 1)
+            attendance_section = create_attendance_section(df)
             
             return html.Div([
                 html.H5(filename),
                 html.H6(datetime.datetime.fromtimestamp(date).strftime('%Y-%m-%d %H:%M:%S')),
-                summary_section,
-                enrolment_section,
-                attendance_section
-            ], style={'padding-left': '20px', 'padding-right': '20px', 'padding-top': '20px'})
-    
+                dbc.Row([
+                    dbc.Col(summary_section, md=8),  # Use 8 out of 12 columns for the summary
+                    dbc.Col(html.Div(), md=4),  # This is to fill the space if you want to maintain the grid layout. Adjust or remove as needed.
+                ]),
+                dbc.Row([
+                    dbc.Col(html.Div(enrolment_section, className="enrolment-container"), md=3),
+                    dbc.Col(html.Div(attendance_section, className="attendance-container"), md=5)
+                ]),
+            ], style={'padding-left': '1.5em', 'padding-right': '1.5em', 'padding-top': '1.5em'})
+
     except Exception as e:
         return html.Div(['There was an error processing this file: {}'.format(e)])
